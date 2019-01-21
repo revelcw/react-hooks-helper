02ca9321176aa259345bba46f59217971cbc109e
donavon <github@donavon.com>
2019-01-21 06:02:14 -0500

    modify tests to use proxyHook

--- __tests__/useNot.test.js
+++ __tests__/useNot.test.js
@@ -1,48 +1,42 @@
-import React from 'react';
-import { render, fireEvent, cleanup } from 'react-testing-library';
+import { proxyHook, cleanup } from 'react-proxy-hook';
 import 'jest-dom/extend-expect';
 
 import { useNot } from '../src';
 
+const useNotProxy = proxyHook(useNot);
+
 afterEach(cleanup);
 
-const Component = ({ initialValue }) => {
-  const [value, notValue] = useNot(initialValue);
-  return <button onClick={notValue}>{`${value}`}</button>;
-};
+const value = 0;
+const notValue = 1;
 
 describe('useNot', () => {
   test('returns the initial value of `true`', () => {
-    const { container } = render(<Component initialValue={true} />);
-    const child = container.firstChild;
-    expect(child.textContent).toBe('true');
+    const returnValue = useNotProxy(true);
+    expect(returnValue[value]).toBe(true);
   });
 
   test('returns the initial value of `true` if truthy value passed', () => {
-    const { container } = render(<Component initialValue="foo" />);
-    const child = container.firstChild;
-    expect(child.textContent).toBe('true');
+    const returnValue = useNotProxy('foo');
+    expect(returnValue[value]).toBe(true);
   });
 
   test('returns the initial value of `false`', () => {
-    const { container } = render(<Component initialValue={false} />);
-    const child = container.firstChild;
-    expect(child.textContent).toBe('false');
+    const returnValue = useNotProxy(false);
+    expect(returnValue[value]).toBe(false);
   });
 
   test('returns the initial value of `false` if no param specified', () => {
-    const { container } = render(<Component />);
-    const child = container.firstChild;
-    expect(child.textContent).toBe('false');
+    const returnValue = useNotProxy();
+    expect(returnValue[value]).toBe(false);
   });
 
   test('toggles from when we call the not function', () => {
-    const { container } = render(<Component />);
-    const child = container.firstChild;
-    expect(child.textContent).toBe('false');
-    fireEvent.click(child);
-    expect(child.textContent).toBe('true');
-    fireEvent.click(child);
-    expect(child.textContent).toBe('false');
+    const returnValue = useNotProxy();
+    expect(returnValue[value]).toBe(false);
+    returnValue[notValue]();
+    expect(returnValue[value]).toBe(true);
+    returnValue[notValue]();
+    expect(returnValue[value]).toBe(false);
   });
 });
--- __tests__/useTrafficLight.test.js
+++ __tests__/useTrafficLight.test.js
@@ -1,53 +1,45 @@
-import React from 'react';
-import { render, cleanup } from 'react-testing-library';
+import { proxyHook, cleanup } from 'react-proxy-hook';
 import 'jest-dom/extend-expect';
 
 import { useTrafficLight } from '../src';
 
+const useTrafficLightProxy = proxyHook(useTrafficLight);
+
 afterEach(cleanup);
 jest.useFakeTimers();
-
-const Component = ({ initialColorIndex, durations }) => {
-  const index = useTrafficLight(initialColorIndex, durations);
-  return <span>{index}</span>;
-};
 
 describe('useTrafficLight', () => {
   test('import { useTrafficLight } from "react-hooks-helper"', () => {
     expect(typeof useTrafficLight).toBe('function');
   });
   test('defaults to an initial index of 0', () => {
-    const { container } = render(<Component />);
-    const child = container.firstChild;
-    expect(child.textContent).toBe('0');
+    const returnValue = useTrafficLightProxy();
+    expect(returnValue.value).toBe(0);
   });
   test('supports a custom initial index of 0, 1, or 2', () => {
     [0, 1, 2].forEach((i) => {
-      const { container } = render(<Component initialColorIndex={i} />);
-      const child = container.firstChild;
-      expect(child.textContent).toBe(`${i}`);
+      const returnValue = useTrafficLightProxy(i);
+      expect(returnValue.value).toBe(i);
     });
   });
   test('has a default duration array of [5000, 4000, 1000] msecs', () => {
-    const { container } = render(<Component />);
-    const child = container.firstChild;
-    expect(child.textContent).toBe('0');
+    const returnValue = useTrafficLightProxy();
+    expect(returnValue.value).toBe(0);
     jest.advanceTimersByTime(5000);
-    expect(child.textContent).toBe('1');
+    expect(returnValue.value).toBe(1);
     jest.advanceTimersByTime(4000);
-    expect(child.textContent).toBe('2');
+    expect(returnValue.value).toBe(2);
     jest.advanceTimersByTime(1000);
-    expect(child.textContent).toBe('0');
+    expect(returnValue.value).toBe(0);
   });
   test('supports a custom duration array', () => {
-    const { container } = render(<Component durations={[500, 400, 100]} />);
-    const child = container.firstChild;
-    expect(child.textContent).toBe('0');
+    const returnValue = useTrafficLightProxy(0, [500, 400, 100]);
+    expect(returnValue.value).toBe(0);
     jest.advanceTimersByTime(500);
-    expect(child.textContent).toBe('1');
+    expect(returnValue.value).toBe(1);
     jest.advanceTimersByTime(400);
-    expect(child.textContent).toBe('2');
+    expect(returnValue.value).toBe(2);
     jest.advanceTimersByTime(100);
-    expect(child.textContent).toBe('0');
+    expect(returnValue.value).toBe(0);
   });
 });

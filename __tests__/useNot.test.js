cd886e16a800c53b3354ded42cbd3062ee35e4cb
donavon <github@donavon.com>
2019-01-23 10:38:10 -0500

    use new testHook for tests

--- __tests__/useNot.test.js
+++ __tests__/useNot.test.js
@@ -1,42 +1,42 @@
-import { proxyHook, cleanup } from 'react-proxy-hook';
+import { testHook, cleanup } from 'react-proxy-hook';
 import 'jest-dom/extend-expect';
 
 import { useNot } from '../src';
 
-const useNotProxy = proxyHook(useNot);
-
 afterEach(cleanup);
-
-const value = 0;
-const notValue = 1;
 
 describe('useNot', () => {
   test('returns the initial value of `true`', () => {
-    const returnValue = useNotProxy(true);
-    expect(returnValue[value]).toBe(true);
+    let value;
+    testHook(() => ([value] = useNot(true)));
+    expect(value).toBe(true);
   });
 
   test('returns the initial value of `true` if truthy value passed', () => {
-    const returnValue = useNotProxy('foo');
-    expect(returnValue[value]).toBe(true);
+    let value;
+    testHook(() => ([value] = useNot('foo')));
+    expect(value).toBe(true);
   });
 
   test('returns the initial value of `false`', () => {
-    const returnValue = useNotProxy(false);
-    expect(returnValue[value]).toBe(false);
+    let value;
+    testHook(() => ([value] = useNot(false)));
+    expect(value).toBe(false);
   });
 
   test('returns the initial value of `false` if no param specified', () => {
-    const returnValue = useNotProxy();
-    expect(returnValue[value]).toBe(false);
+    let value;
+    testHook(() => ([value] = useNot()));
+    expect(value).toBe(false);
   });
 
   test('toggles from when we call the not function', () => {
-    const returnValue = useNotProxy();
-    expect(returnValue[value]).toBe(false);
-    returnValue[notValue]();
-    expect(returnValue[value]).toBe(true);
-    returnValue[notValue]();
-    expect(returnValue[value]).toBe(false);
+    let value, notValue;
+    testHook(() => ([value, notValue] = useNot()));
+    expect(value).toBe(false);
+    notValue();
+    expect(value).toBe(true);
+    notValue();
+    expect(value).toBe(false);
   });
 });
--- __tests__/useStep.test.js
+++ __tests__/useStep.test.js
@@ -1,9 +1,7 @@
-import { proxyHook, cleanup } from 'react-proxy-hook';
+import { testHook, cleanup } from 'react-proxy-hook';
 import 'jest-dom/extend-expect';
 
 import { useStep } from '../src';
-
-const useStepProxy = proxyHook(useStep);
 
 afterEach(cleanup);
 jest.useFakeTimers();
@@ -13,92 +11,104 @@ describe('useStep', () => {
     expect(typeof useStep).toBe('function');
   });
   test('defaults to an initial index of 0', () => {
-    const returnValue = useStepProxy({ steps: 3 });
-    expect(returnValue.index).toBe(0);
+    let index;
+    testHook(() => ({ index } = useStep({ steps: 3 })));
+    expect(index).toBe(0);
   });
   test('can be passed an `initialStep` as a number)', () => {
-    const returnValue = useStepProxy({ steps: 3, initialStep: 1 });
-    expect(returnValue.index).toBe(1);
+    let index;
+    testHook(() => ({ index } = useStep({ steps: 3, initialStep: 1 })));
+    expect(index).toBe(1);
   });
   test('can be passed an `initialStep` as a string', () => {
     expect(() => {
-      useStepProxy({ steps: 3, initialStep: 'foo' });
+      testHook(() => (useStep({ steps: 3, initialStep: 'foo' })));
     }).toThrow();
   });
   test('can be passed `steps` as a number', () => {
-    const returnValue = useStepProxy({ steps: 3, initialStep: 1 });
-    expect(returnValue.step).toEqual({});
+    let step;
+    testHook(() => ({ step } = useStep({ steps: 3, initialStep: 1 })));
+    expect(step).toEqual({});
   });
   test('can be passed `steps` as an array', () => {
-    const returnValue = useStepProxy({ initialStep: 1, steps: [{ foo: 'foo' }, { foo: 'bar' }] });
-    expect(returnValue.step).toEqual({ foo: 'bar' });
+    let step;
+    testHook(() => ({ step } = useStep({ initialStep: 1, steps: [{ foo: 'foo' }, { foo: 'bar' }] })));
+    expect(step).toEqual({ foo: 'bar' });
   });
   test('errors if `steps` is not a number or an array', () => {
     expect(() => {
-      useStepProxy({ steps: 'foo', initialStep: 1 });
+      testHook(() => useStep({ steps: 'foo', initialStep: 1 }));
     }).toThrow();
   });
   test('can be passed an optional `autoAdvanceDuration`', () => {
-    const returnValue = useStepProxy({ steps: 3, autoAdvanceDuration: 500 });
-    expect(returnValue.index).toBe(0);
+    let index;
+    testHook(() => ({ index } = useStep({ steps: 3, autoAdvanceDuration: 500 })));
+    expect(index).toBe(0);
     jest.advanceTimersByTime(500);
-    expect(returnValue.index).toBe(1);
+    expect(index).toBe(1);
   });
   test('you can call `next` to advance to the next step (steps wrap)', () => {
-    const returnValue = useStepProxy({ steps: 3, initialStep: 1 });
-    expect(returnValue.index).toBe(1);
-    returnValue.navigation.next();
-    expect(returnValue.index).toBe(2);
-    returnValue.navigation.next();
-    expect(returnValue.index).toBe(0);
+    let index, navigation;
+    testHook(() => ({ index, navigation } = useStep({ steps: 3, initialStep: 1 })));
+    navigation.next();
+    expect(index).toBe(2);
+    navigation.next();
+    expect(index).toBe(0);
   });
   test('you can call `previous` to advance to the previous step (steps wrap)', () => {
-    const returnValue = useStepProxy({ steps: 3, initialStep: 1 });
-    expect(returnValue.index).toBe(1);
-    returnValue.navigation.previous();
-    expect(returnValue.index).toBe(0);
-    returnValue.navigation.previous();
-    expect(returnValue.index).toBe(2);
+    let index, navigation;
+    testHook(() => ({ index, navigation } = useStep({ steps: 3, initialStep: 1 })));
+    expect(index).toBe(1);
+    navigation.previous();
+    expect(index).toBe(0);
+    navigation.previous();
+    expect(index).toBe(2);
   });
   test('you can call `go` to go to a specific index', () => {
-    const returnValue = useStepProxy({ steps: 3 });
-    expect(returnValue.index).toBe(0);
-    returnValue.navigation.go(1);
-    expect(returnValue.index).toBe(1);
+    let index, navigation;
+    testHook(() => ({ index, navigation } = useStep({ steps: 3 })));
+    expect(index).toBe(0);
+    navigation.go(1);
+    expect(index).toBe(1);
   });
   test('you can call `go` to go to a specific `step.id`', () => {
-    const returnValue = useStepProxy({ steps: [{ id: 'bar' }, { id: 'foo' }] });
-    expect(returnValue.index).toBe(0);
-    returnValue.navigation.go('foo');
-    expect(returnValue.index).toBe(1);
+    let index, navigation;
+    testHook(() => ({ index, navigation } = useStep({ steps: [{ id: 'bar' }, { id: 'foo' }] })));
+    expect(index).toBe(0);
+    navigation.go('foo');
+    expect(index).toBe(1);
   });
   test('errors of you can call `go` with an index out of range', () => {
-    const returnValue = useStepProxy({ steps: 3 });
+    let navigation;
+    testHook(() => ({ navigation } = useStep({ steps: 3 })));
     expect(() => {
-      returnValue.navigation.go(42);
+      navigation.go(42);
     }).toThrow();
   });
   test('errors of you can call `go` with an `id` and the `step.id` is not found', () => {
-    const returnValue = useStepProxy({ steps: 3 });
+    let navigation;
+    testHook(() => ({ navigation } = useStep({ steps: 3 })));
     expect(() => {
-      returnValue.navigation.go('foo');
+      navigation.go('foo');
     }).toThrow();
   });
   test('you can call `pause` to pause auto advance', () => {
-    const returnValue = useStepProxy({ steps: 3, autoAdvanceDuration: 500 });
-    expect(returnValue.index).toBe(0);
+    let index, navigation;
+    testHook(() => ({ index, navigation } = useStep({ steps: 3, autoAdvanceDuration: 500 })));
+    expect(index).toBe(0);
     jest.advanceTimersByTime(250);
-    returnValue.navigation.pause();
+    navigation.pause();
     jest.advanceTimersByTime(250);
-    expect(returnValue.index).toBe(0);
+    expect(index).toBe(0);
   });
   test('you can call `play` to restart auto advance', () => {
-    const returnValue = useStepProxy({ steps: 3, autoAdvanceDuration: 500 });
-    expect(returnValue.index).toBe(0);
+    let index, navigation;
+    testHook(() => ({ index, navigation } = useStep({ steps: 3, autoAdvanceDuration: 500 })));
+    expect(index).toBe(0);
     jest.advanceTimersByTime(250);
-    returnValue.navigation.pause();
-    returnValue.navigation.play();
+    navigation.pause();
+    navigation.play();
     jest.advanceTimersByTime(500);
-    expect(returnValue.index).toBe(1);
+    expect(index).toBe(1);
   });
 });

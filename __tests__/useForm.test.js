8ef486d6e751fc6dbd01d0c8646150259b5ccd6c
Donavon West <github@donavon.com>
2019-01-23 10:42:41 -0500

    use new testHook for tests (#5)

--- __tests__/useForm.test.js
+++ __tests__/useForm.test.js
@@ -1,9 +1,7 @@
-import { proxyHook, cleanup } from 'react-proxy-hook';
+import { testHook, cleanup } from 'react-proxy-hook';
 import 'jest-dom/extend-expect';
 
 import { useForm } from '../src';
-
-const useFormProxy = proxyHook(useForm);
 
 afterEach(cleanup);
 
@@ -21,34 +19,36 @@ const initialValuesNested = {
   },
 };
 
-const formData = 0;
-const setForm = 1;
-
 describe('useForm', () => {
   test('returns the initial values set in defaultValues', () => {
-    const form = useFormProxy(initialValues);
-    expect(form[formData].foo).toBe('foo');
-    expect(form[formData].isBar).toBe(false);
+    let foo, isBar;
+    testHook(() => ([{ foo, isBar }] = useForm(initialValues)));
+    expect(foo).toBe('foo');
+    expect(isBar).toBe(false);
   });
 
   test('returns changes a text field when we fire an onChange', () => {
-    const form = useFormProxy(initialValues);
-    form[setForm]({ target: { type: 'text', name: 'foo', value: 'bar' } });
-    expect(form[formData].foo).toBe('bar');
+    let foo, setForm;
+    testHook(() => ([{ foo }, setForm] = useForm(initialValues)));
+    const event = { target: { type: 'text', name: 'foo', value: 'bar' } };
+    setForm(event);
+    expect(foo).toBe('bar');
   });
 
   test('returns changes a checkbox when we fire an onChange', () => {
-    const form = useFormProxy(initialValues);
-    form[setForm]({ target: { type: 'checkbox', name: 'isBar', checked: true } });
-    expect(form[formData].isBar).toBe(true);
+    let isBar, setForm;
+    testHook(() => ([{ isBar }, setForm] = useForm(initialValues)));
+    const event = { target: { type: 'checkbox', name: 'isBar', checked: true } };
+    setForm(event);
+    expect(isBar).toBe(true);
   });
 
   test('supports nested paths for names', () => {
-    const form = useFormProxy(initialValuesNested);
-    expect(form[formData].foo.bar).toBe('bar');
-    expect(form[formData].foo2.bar).toBe('bar');
-    form[setForm]({ target: { type: 'text', name: 'foo.bar', value: 'baz' } });
-    expect(form[formData].foo.bar).toBe('baz');
-    expect(form[formData].foo2.bar).toBe('bar');
+    let foo, foo2, setForm;
+    testHook(() => ([{ foo, foo2 }, setForm] = useForm(initialValuesNested)));
+    const event = { target: { type: 'text', name: 'foo.bar', value: 'baz' } };
+    setForm(event);
+    expect(foo.bar).toBe('baz');
+    expect(foo2.bar).toBe('bar');
   });
 });
--- __tests__/useTrafficLight.test.js
+++ __tests__/useTrafficLight.test.js
@@ -1,9 +1,7 @@
-import { proxyHook, cleanup } from 'react-proxy-hook';
+import { testHook, cleanup } from 'react-proxy-hook';
 import 'jest-dom/extend-expect';
 
 import { useTrafficLight } from '../src';
-
-const useTrafficLightProxy = proxyHook(useTrafficLight);
 
 afterEach(cleanup);
 jest.useFakeTimers();
@@ -13,33 +11,37 @@ describe('useTrafficLight', () => {
     expect(typeof useTrafficLight).toBe('function');
   });
   test('defaults to an initial index of 0', () => {
-    const returnValue = useTrafficLightProxy();
-    expect(returnValue.value).toBe(0);
+    let value;
+    testHook(() => (value = useTrafficLight()));
+    expect(value).toBe(0);
   });
   test('supports a custom initial index of 0, 1, or 2', () => {
     [0, 1, 2].forEach((i) => {
-      const returnValue = useTrafficLightProxy(i);
-      expect(returnValue.value).toBe(i);
+      let value;
+      testHook(() => (value = useTrafficLight(i)));
+      expect(value).toBe(i);
     });
   });
   test('has a default duration array of [5000, 4000, 1000] msecs', () => {
-    const returnValue = useTrafficLightProxy();
-    expect(returnValue.value).toBe(0);
+    let value;
+    testHook(() => (value = useTrafficLight()));
+    expect(value).toBe(0);
     jest.advanceTimersByTime(5000);
-    expect(returnValue.value).toBe(1);
+    expect(value).toBe(1);
     jest.advanceTimersByTime(4000);
-    expect(returnValue.value).toBe(2);
+    expect(value).toBe(2);
     jest.advanceTimersByTime(1000);
-    expect(returnValue.value).toBe(0);
+    expect(value).toBe(0);
   });
   test('supports a custom duration array', () => {
-    const returnValue = useTrafficLightProxy(0, [500, 400, 100]);
-    expect(returnValue.value).toBe(0);
+    let value;
+    testHook(() => (value = useTrafficLight(0, [500, 400, 100])));
+    expect(value).toBe(0);
     jest.advanceTimersByTime(500);
-    expect(returnValue.value).toBe(1);
+    expect(value).toBe(1);
     jest.advanceTimersByTime(400);
-    expect(returnValue.value).toBe(2);
+    expect(value).toBe(2);
     jest.advanceTimersByTime(100);
-    expect(returnValue.value).toBe(0);
+    expect(value).toBe(0);
   });
 });
